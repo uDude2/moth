22a19289cfe87e857bc90003b3fb8c0d8c1e70c3
Neale Pickett <neale@woozle.org>
2017-07-15 23:04:05 -0600

    Making db connection and serving content

--- moth.py
+++ moth.py
@@ -1,7 +1,42 @@
 #! /usr/bin/python2
 
 import string
+import MySQLdb
+import os
 import webapp2
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
 
 class Page:
   def __init__(self, title, template="template.html"):
@@ -25,17 +60,32 @@ class Page:
       content="\n".join(self.content),
     )
 
+
 class Home(webapp2.RequestHandler):
   def get(self):
     p = Page("Fire Home")
     p.write("Hello, webapp2!")
     self.response.write(p)
+
     
 class Register(webapp2.RequestHandler):
-  pass
+	def get(self):
+		p = Page("Teams")
+		
+		p.write("<pre>")
+		db = connect_to_cloudsql()
+		db.select_db("moth_spark")
+		cursor = db.cursor()
+		cursor.execute("select * from teams")
+		for r in cursor.fetchall():
+			p.write("{}\n".format(r))
+			
+		self.response.write(p)
+
 
 class Puzzler(webapp2.RequestHandler):
   pass
+
 
 class TokenRedeemer(webapp2.RequestHandler):
   pass
